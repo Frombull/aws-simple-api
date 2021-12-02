53fa19e3498d5d75af6561115be87d30a74942bd
Frombull <marcorrditoro@gmail.com>
2021-12-02 01:23:28 -0300

    Initial commit

---
+++ dynamo_functions.py
@@ -0,0 +1,60 @@
+import json
+
+import boto3
+from botocore.config import Config
+#from botocore.exceptions import ClientError
+
+from logger import ColorLog
+
+
+my_config = Config(
+    region_name='sa-east-1',
+    signature_version='v4',
+    retries={
+        'max_attempts': 3,
+        'mode': 'standard'
+    }
+)
+TABLE = 'persons-table'
+dynamodb = boto3.client('dynamodb', config=my_config)
+
+log = ColorLog()
+
+
+def savePerson(*, first_name: str, last_name: str, email: str) -> dict:
+    response = dynamodb.put_item(TableName=TABLE,
+                                 Item={
+                                     'firstName': {'S': first_name},
+                                     'lastName': {'S': last_name},
+                                     'email': {'S': email}
+                                 })
+
+    http_code = response["ResponseMetadata"]["HTTPStatusCode"]
+    if http_code == 200:
+        log.info('Item added successfuly!')
+    else:
+        log.error(f'{http_code}, Error adding item to DB ')
+    return response
+
+
+def getPerson(*, first_name: str, last_name: str):
+    response = dynamodb.get_item(TableName=TABLE,
+                                 Key={
+                                    'firstName': {'S': first_name},
+                                    'lastName': {'S': last_name},
+                                 })
+
+    http_code = response['ResponseMetadata']['HTTPStatusCode']
+    if http_code == 200:
+        log.info('Item retrieved successfuly!')
+    else:
+        log.error(f'{http_code}, Error retrieving item from DB ')
+
+    return response
+
+
+if __name__ == '__main__':
+    #savePerson(first_name='Bãojão', last_name='Do pão', email='teupai@gmail.com')
+    #getPerson(first_name='Marco', last_name='Di Toro')
+    pass
+
---
+++ process_person_lambda.py
@@ -0,0 +1,38 @@
+import json
+
+from logger import ColorLog
+
+log = ColorLog()
+log.info('Loading lambda function')
+
+
+def lambda_handler(event, context):
+    # Parse out query string params
+    rawPath = event['rawPath']
+    firstName = event['queryStringParameters']['firstName']
+    lastName = event['queryStringParameters']['lastName']
+    email = event['queryStringParameters']['email']
+
+    # Process data
+    path = event['rawPath'].lower()
+
+    if path(event) == 'get':
+        pass
+        # TODO: Get item from db
+
+    elif path(event) == 'create':
+        pass
+        # TODO: Create item in db
+
+
+    # TODO: Create http response object
+    responseObject = {
+        "statusCode": http_code,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(response_body)
+    }
+
+    # 4. Return
+    return responseObject
